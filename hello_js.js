125c3e8e64009db3c3cf69204969a1ecac0d385b
BlueSpinney <99137204+BlueSpinney@users.noreply.github.com>
2022-04-19 23:55:36 +0200

    Add files via upload

--- hello_js.js
+++ hello_js.js
@@ -1,5 +1,29 @@
-const b1 = document.querySelector('button');
+let b2 = document.createElement("button")
+b2.innerHTML = "I like you, you are handsome and a great person please come back we miss you, wake up you have been in coma for 30 years now "
+document.body.appendChild(b2)
+
+
+const b1 = document.querySelector('button')
+const p = document.getElementById('pa')
+let variable = 0
+var vid = document.getElementById("chad");
+
 
 b1.onclick = () => {
-    alert('hi mom')
+    if (variable === 0) {
+        const b1 = document.querySelector('button')
+        
+        document.body.appendChild(b2)
+        p.textContent = "sog"
+        variable = 1
+
+    }
+    else if (variable === 1){
+        const b1 = document.querySelector('button')
+
+        document.body.appendChild(b2)
+        p.textContent = "pog"
+        variable = 0
+    }
+
 }
